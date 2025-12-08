4a98e38dc1f0d3f35d56cbdee8851ee55d4b490b
Jean Sacren <sakiwit@gmail.com>
2025-12-07 22:51:50 -0700

    Fix 2FA fields in UserFactory
    Set two-factor authentication fields to null by default so that 2FA is
    opt-in and should only be enabled via the authenticated user interface
    (e.g., Fortify flow).  This prevents invalid 2FA state in seeded or
    factory-created users.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,9 +29,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'two_factor_secret' => Str::random(10),
-            'two_factor_recovery_codes' => Str::random(10),
-            'two_factor_confirmed_at' => now(),
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'two_factor_confirmed_at' => null,
         ];
     }
 
